f6f566af383e83a17aa4cbc2a76ad63d2c944501
Vivekdhore2025 <vivekdhore2025@gmail.com>
2025-02-11 00:55:00 +0530

    Add files via upload

---
+++ Program2.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+// Problem Statement : Print HelloWorld in Code
+
+// Step1 : Undestand the problem statement
+// Conclusion : We are going to use the Printf Fuction to Display in code
+
+// Step 2 : Algorithm
+/*
+    START
+        
+     we use the Printf Function to Display on Screen
+    STOP
+*/  
+
+
+///////////////////////////////////////////////////////////////////////////
+//
+//  Function name :     Main
+//  Description :       It is used to print Hello World in code
+//  Input :              Character
+//  Output :            character
+//  Auther name :        Vivek Haridas Dhore
+//  Date :              02/1/2025
+//
+///////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//  Entry Point function
+//
+///////////////////////////////////////////////////////////////////////////
+int main()
+{
+
+    printf(" Hello World...!");
+
+
+return 0;
+}
